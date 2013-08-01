c4a4407e0e75b599dd6affeaabb1c703c1b6fb2b
luke hartman <luke@mrturtle.com>
2013-07-31 22:09:58 -0500

    Initial files for verification emails

---
+++ app/mailers/household_mailer.rb
@@ -0,0 +1,9 @@
+class HouseholdMailer < ActionMailer::Base
+  default from: Rails.configuration.email_from
+  
+  def verify_information(household)
+      @household = household
+      recipients = [@household.email, @household.adult_one_email, @household.adult_two_email].compact.join(",")
+      mail(to: recipients, subject: Rails.configuration.email_subject)
+    end
+end
---
+++ test/functional/household_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HouseholdMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
