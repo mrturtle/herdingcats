c1a1b784d6a952849baaca419146d9ec579e386b
luke hartman <luke@mrturtle.com>
2013-07-31 23:24:19 -0500

    Added show_information boolean to household

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130727171516) do
+ActiveRecord::Schema.define(:version => 20130801041747) do
 
   create_table "households", :force => true do |t|
     t.string   "street"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20130727171516) do
     t.boolean  "adult_two_text"
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
+    t.boolean  "show_information"
   end
 
   create_table "school_classes", :force => true do |t|
