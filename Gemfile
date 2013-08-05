8f0b908f047517fc4052160c1e7f8355482c451c
Luke Hartman <luke@mrturtle.com>
2013-08-05 15:39:29 -0500

    Changed gem from maildown to markerb

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'sqlite3'
 gem 'thin'
 gem "mail"
 gem 'choices'
-#gem 'maildown'
+gem 'markerb'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/mailers/household_mailer.rb
+++ app/mailers/household_mailer.rb
@@ -4,6 +4,9 @@ class HouseholdMailer < ActionMailer::Base
   def verify_information(household)
       @household = household
       recipients = [@household.email, @household.adult_one_email, @household.adult_two_email].compact.join(",")
-      mail(to: recipients, subject: Rails.configuration.email_subject)
+      mail(to: recipients, subject: Rails.configuration.email_subject) do |format|
+        format.text
+        format.html
+      end
     end
 end
